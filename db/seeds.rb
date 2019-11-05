516738a7f79f8e2dcaf9cb04b90574dc1ac3e86d
Janae Hall <janaeelisa@gmail.com>
2019-11-05 14:43:09 -0600

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Genre.create([{name: "Pop"}, {name: "Rock"}, {name: "Hip Hop"}, {name: "Rap"}])
